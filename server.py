51c1c0a115c28af2d03dc02245b0534064af55a0
TrentaIcedCoffee <jin@ucdavis.edu>
2020-02-09 06:47:36 +0000

    socket cors

--- server.py
+++ server.py
@@ -52,11 +52,11 @@ def updateSession(user):
   session['userId'] = user.userId
   session['username'] = user.username
 
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('connectEvent')
 def connect():
   emit('status', {'message': 'connected'})
 
 if __name__ == '__main__':
-  socketio.run(app, host='localhost')
+  socketio.run(app, host='0.0.0.0', port=3000)
