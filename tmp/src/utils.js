5b9c56d13a3ce8f9309e31a8dc35febeef2e09ad
Bingxuan Ying <yingbingxuan@gmail.com>
2020-02-27 01:51:47 -0800

    sync

--- tmp/src/utils.js
+++ tmp/src/utils.js
@@ -1,19 +1,19 @@
-import app from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-export { default as SocketIOClient } from 'socket.io-client';
+import app from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+export { default as SocketIOClient } from "socket.io-client";
 
 const firebaseConfig = {
- apiKey: "AIzaSyC0IrR4AvUJVRX_aUyucjhV66YzawPKYFk",
+  apiKey: "AIzaSyC0IrR4AvUJVRX_aUyucjhV66YzawPKYFk",
   authDomain: "chat-3809f.firebaseapp.com",
   databaseURL: "https://chat-3809f.firebaseio.com",
   projectId: "chat-3809f",
   storageBucket: "chat-3809f.appspot.com",
   messagingSenderId: "204136491965",
-  appId: "1:204136491965:web:86dea458298aa68ca2c0d4",
+  appId: "1:204136491965:web:86dea458298aa68ca2c0d4"
 };
 
 export const firebase = app.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
 export const auth = firebase.auth();
-export const endpoint = 'http://54.210.126.161:3000';
+export const endpoint = "http://54.210.126.161:3000";
